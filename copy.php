27570805ad1a2a139ec0f798e22ad7daf3366848
Marius de Beer <53187487+mariusdebeer@users.noreply.github.com>
2023-08-24 08:51:36 +0200

    Updated to add error handling.

--- copy.php
+++ copy.php
@@ -1,25 +1,46 @@
 <?php
-set_time_limit(0); //Unlimited max execution time
+set_time_limit(0); // Unlimited max execution time
+
 $path = 'newfile.zip';
 $url = 'http://www.path-to-old-file.com/backup.zip';
 $newfname = $path;
-echo 'Starting Download!
-';
-$file = fopen ($url, "rb");
-if($file) {
-$newf = fopen ($newfname, "wb");
-if($newf)
-while(!feof($file)) {
-fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 );
-echo '1 MB File Chunk Written!
-';
+
+echo 'Starting Download!' . PHP_EOL;
+
+// Open a remote connection to the URL
+$file = fopen($url, "rb");
+if (!$file) {
+    die('Error: Unable to open remote file for reading.');
 }
+
+// Open the local file for writing
+$newf = fopen($newfname, "wb");
+if (!$newf) {
+    fclose($file);
+    die('Error: Unable to open local file for writing.');
 }
-if($file) {
+
+// Download and write file in chunks
+while (!feof($file)) {
+    $chunk = fread($file, 1024 * 8);
+    if ($chunk === false) {
+        fclose($file);
+        fclose($newf);
+        die('Error: Failed to read remote file.');
+    }
+
+    $bytesWritten = fwrite($newf, $chunk);
+    if ($bytesWritten === false) {
+        fclose($file);
+        fclose($newf);
+        die('Error: Failed to write to local file.');
+    }
+}
+
+// Close file pointers
 fclose($file);
-}
-if($newf) {
 fclose($newf);
-}
-echo 'Finished!';
+
+echo 'Download finished!' . PHP_EOL;
 ?>
+
